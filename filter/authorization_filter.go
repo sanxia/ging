d344377c8f761f732415482fcd983482f5ee8606
liuming <liuming@chinawlx.com>
2017-05-22 11:53:03 +0800

    controller update

---
+++ filter/authorization_filter.go
@@ -0,0 +1,88 @@
+package filter
+
+import (
+	"log"
+	"net/url"
+	"time"
+)
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+import (
+	"github.com/sanxia/ging"
+	"github.com/sanxia/ging/util"
+)
+
+/* ================================================================================
+ * 授权过滤器
+ * author: mliu
+ * ================================================================================ */
+type authorizationFilter struct {
+	Filter
+	AuthorizationOption
+}
+
+type AuthorizationOption struct {
+	Authorization *func(*ging.UserIdentity) bool
+	Role          string
+	AuthUrl       string
+}
+
+func AuthorizationFilter(authorizationOption *AuthorizationOption) IActionFilter {
+	return &authorizationFilter{
+		Filter: Filter{
+			Name: "authorization",
+		},
+		AuthorizationOption: AuthorizationOption{
+			Authorization: authorizationOption.Authorization,
+			Role:          authorizationOption.Role,
+			AuthUrl:       authorizationOption.AuthUrl,
+		},
+	}
+}
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * 动作执行之前
+ * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+func (s *authorizationFilter) Before(ctx *gin.Context) result.IActionResult {
+	log.Printf("[%s] Before %v", s.Name, time.Now())
+
+	requestUrl := ctx.Request.URL.RequestURI()
+	var actionResult ging.IActionResult
+	var userIdentity *ging.UserIdentity
+	if identity, isOk := ctx.Get(ging.UserIdentityKey); isOk {
+		user := identity.(ging.UserIdentity)
+		userIdentity = &user
+	}
+
+	//跳转到登录地址
+	authUrl := s.AuthUrl
+	authUrl += "?returnurl=" + url.QueryEscape(requestUrl)
+	if userIdentity != nil {
+		//Authorization优先于Role
+		if s.Authorization != nil {
+			authorization := *s.Authorization
+			if !authorization(userIdentity) {
+				actionResult = result.RedirectResult(ctx, authUrl)
+			}
+		} else {
+			isInRole := util.IsInRole(userIdentity.Role, s.Role)
+			if !isInRole {
+				actionResult = result.RedirectResult(ctx, authUrl)
+			}
+		}
+	} else {
+		actionResult = result.RedirectResult(ctx, authUrl)
+	}
+
+	return actionResult
+}
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * 动作执行之后
+ * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+func (s *authorizationFilter) After(ctx *gin.Context) {
+	log.Printf("[%s] After %v", s.Name, time.Now())
+}
---
+++ result.go
@@ -0,0 +1,134 @@
+package ging
+
+import (
+	"io"
+)
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+import (
+	"github.com/sanxia/ging/render"
+)
+
+/* ================================================================================
+ * 动作结果
+ * author: 美丽的地球啊
+ * ================================================================================ */
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * 动作结果接口
+ * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+type IActionResult interface {
+	Render()
+}
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * 视图结果数据结构
+ * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+type (
+	ActionResult struct {
+		context     *gin.Context
+		data        interface{}
+		contentType string
+		statusCode  int
+		isAbort     bool
+	}
+)
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * 渲染Html模版
+ * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+func (result *ActionResult) Html(template string, args ...interface{}) {
+	if result.context.IsAborted() {
+		return
+	}
+
+	//result.context.Writer.WriteHeader(200)
+	//result.context.Writer.WriteString("hahah liuming")
+	//result.context.Writer.Write([]byte("2016"))
+	//result.context.WriteHeader(w.status)
+	//result.context.Abort()
+
+	render.Html(result.context, template, args...)
+}
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * 渲染Json字符串
+ * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+func (result *ActionResult) Json(args ...interface{}) {
+	if result.context.IsAborted() {
+		return
+	}
+	render.Json(result.context, args...)
+}
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * 渲染字符串
+ * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+func (result *ActionResult) String(args ...interface{}) {
+	if result.context.IsAborted() {
+		return
+	}
+
+	render.String(result.context, args...)
+}
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * 渲染Xml
+ * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+func (result *ActionResult) Xml(args ...interface{}) {
+	if result.context.IsAborted() {
+		return
+	}
+
+	render.Xml(result.context, args...)
+}
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * 渲染磁盘物理文件
+ * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+func (result *ActionResult) File(filepath string) {
+	if result.context.IsAborted() {
+		return
+	}
+
+	render.File(result.context, filepath)
+}
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * 渲染字节数据
+ * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+func (result *ActionResult) Data(data []byte, args ...interface{}) {
+	if result.context.IsAborted() {
+		return
+	}
+
+	render.Data(result.context, data, args...)
+}
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * 渲染io数据流
+ * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+func (result *ActionResult) Stream(step func(w io.Writer) bool) {
+	if result.context.IsAborted() {
+		return
+	}
+
+	render.Stream(result.context, step)
+}
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * 渲染io数据流
+ * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+func (result *ActionResult) Error(msg string) {
+	result.String(msg, 400)
+}
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * 渲染err数据流
+ * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+func (result *ActionResult) Redirect(url string) {
+	render.Redirect(result.context, url)
+}
